674d2e56c9f5e6860e0e84826d05ef2c951aa18a
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-05-22 11:54:08 +0530

    Update Before_inst.sh

--- script/Before_inst.sh
+++ script/Before_inst.sh
@@ -2,7 +2,7 @@
 if [ -d /home/ubuntu/new_chatapp/fundoo ]; then
    sudo rm -rf /home/ubuntu/new_chatapp/fundoo
 fi
-if [ -d /home/ubuntu/new_chatapp/requirements.txt]; then
+if [ -f /home/ubuntu/new_chatapp/requirements.txt ]; then
     sudo rm -rf /home/ubuntu/new_chatapp/requirements.txt
 fi    
 
