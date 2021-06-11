b1ca1ad63e0d4e416c18ad8119b03214a1d8b12b
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-11 09:20:35 +0530

    Update move.sh

--- script/move.sh
+++ script/move.sh
@@ -2,6 +2,6 @@
    sudo mv /home/ubuntu/chatapp/.idea /home/ubuntu/new_chatapp
    sudo mv /home/ubuntu/chatapp/.github /home/ubuntu/new_chatapp
    sudo mv /home/ubuntu/chatapp/appspec.yml /home/ubuntu/new_chatapp
-   sudo mv /home/ubuntu/chatapp/scripts /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/chatapp/script /home/ubuntu/new_chatapp
    sudo mv /home/ubuntu/chatapp/fundoo /home/ubuntu/new_chatapp
    sudo mv /home/ubuntu/chatapp/requirements.txt /home/ubuntu/new_chatapp
