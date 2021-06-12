66de6f6838b34215ef3a52b83662db4cd1efb0e0
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-12 13:31:21 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,25 +7,8 @@ pipeline {
                 git credentialsId: 'b9de33bf-5297-40bb-97ff-5a6a8b5a6f67', url: 'https://github.com/ShivDj/new_chatapp.git'
             }
         }
-        stage('build && SonarQube analysis') {
-            steps {
-                withSonarQubeEnv('My SonarQube Server') {
-                    // Optionally use a Maven environment you've configured already
-                    withMaven(maven:'Maven 3.5') {
-                        sh 'mvn clean package sonar:sonar'
-                    }
-                }
-            }
-        }
-        stage("Quality Gate") {
-            steps {
-                timeout(time: 1, unit: 'HOURS') {
-                    // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
-                    // true = set pipeline to UNSTABLE, false = don't
-                    waitForQualityGate abortPipeline: true
-                }
-            }
-        }
+        
+        
         stage('Build') {
             steps {
                 
