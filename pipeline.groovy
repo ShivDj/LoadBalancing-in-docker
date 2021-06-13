0b04a078026938ee9be938e5f18841dde24e3d28
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-13 22:21:24 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -29,7 +29,6 @@ pipeline {
         stage('Deploy') {
             steps {
                sh 'ssh -i /var/lib/jenkins/public_instance_key.pem ubuntu@10.0.1.10 "bash /home/ubuntu/new_chatapp/script/start-server.sh"'
-
             }
         }
     }   
