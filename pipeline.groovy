605d9f65edc42758d08afa1319d6f2fa7b00e6b0
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-15 08:51:49 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -18,7 +18,7 @@ pipeline {
         
         stage ("Quality gate") {
             steps {
-                timeout(time: 3, unit: 'MINUTES') {
+                timeout(time: 5, unit: 'MINUTES') {
                 timeout(time: 1, unit: 'HOURS') {
                     waitForQualityGate abortPipeline: true  
                 }
