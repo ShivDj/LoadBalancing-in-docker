fce5afc0926cbbb3e744534bc1ab5f16458fb0f2
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-24 11:30:29 +0530

    Update start-server.sh

--- script/start-server.sh
+++ script/start-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo systemctl daemon-reload
-sudo systemctl start chatapp
-sudo systemctl restart chatapp
+#sudo systemctl daemon-reload
+#sudo systemctl start chatapp
+#sudo systemctl restart chatapp
+sudo docker-compose up --build -d
