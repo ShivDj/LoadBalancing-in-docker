a602835ba84cc1522224b50b90e147cf81664e56
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-25 09:08:17 +0530

    Update move.sh

--- script/move.sh
+++ script/move.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
-   sudo mv /home/ubuntu/Docker/fundoo /home/ubuntu/Docker_Project
-   sudo mv /home/ubuntu/Docker/script /home/ubuntu/Docker_Project
-   #sudo mv /home/ubuntu/chatapp/appspec.yml /home/ubuntu/new_chatapp
-   #sudo mv /home/ubuntu/chatapp/script /home/ubuntu/new_chatapp
-   #sudo mv /home/ubuntu/chatapp/fundoo /home/ubuntu/new_chatapp
-   #sudo mv /home/ubuntu/chatapp/requirements.txt /home/ubuntu/new_chatapp
+#!/bin/bash  
+  sudo mv /home/ubuntu/Docker/.idea /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/.github /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/script /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/appspec.yml /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/Dockerfile /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/docker-compose.yml /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/pipeline.groovy /home/ubuntu/Docker_Project
+  #sudo mv /home/ubuntu/Docker/sonar-project.properties /home/ubuntu/Docker_Project
+  #sudo mv /home/ubuntu/Docker/appspec.yml /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/frontend_Docker /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/db_docker /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/backend /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker/fundoo /home/ubuntu/Docker_Project
+  sudo mv /home/ubuntu/Docker//requirements.txt /home/ubuntu/Docker_Project
